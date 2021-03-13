58caffe604f1a680d9c94d66ed251a39257e8467
Number6174 <71176286+Number6174@users.noreply.github.com>
2021-03-13 09:53:30 -0700

    Add timer support to server
    Still need to update timer.html to utilize timer_data.json

--- README.md
+++ README.md
@@ -1,25 +1,70 @@
 # WritingHTTPServer
-A simple Python HTTP server that writes GET parameters to a specified file
+A simple Python HTTP server that writes GET parameters to a specified file.
+It also supports a timer that can be extended by specific events.
 
 WARNING: This is a very purpose driven program. If you don't completely understand how to use it, it could expose you to security vulnerabilities. Do not open this program to the broader internet. Only allow it to accept local connections.
 
-By default it only listens on 127.0.0.1
+The provided configuration has it listens only on 127.0.0.1. It is not recommended that you change this.
 
 # Setup
+
 1. Install Python for [Windows](https://www.python.org/downloads/windows/) (tested with 3.9.2)
 2. Verify Python is in your PATH. This can be done by running
 `python --version` in the Windows Terminal. You should see a response like `Python 3.9.2`
+3. Install dateutil. This is easiest to do by typing `pip install python-dateutil` at the command line.
+
+# Configuration
+
+Edit the included `config.json`. The program must be restarted to load any configuration changes
+
+* "host" - This defines the host to listen to. It is strongly recommended to only be 127.0.0.1
+* "port" - The port to listen on. The default configuration uses 8001.
+
+All the dates and times go through [dateutils's parser](https://dateutil.readthedocs.io/en/stable/parser.html)
+which is very generous on formatting. Valid formats include: "5pm", "17:00", "2021-03-14-05", "March 14 at 5pm",
+"3 hours", "3h", "1 minute", "1m", "30 seconds", "30s", "1m30s", among others.
+
+Under timer, you'll find:
+
+* "stream-start" - When your stream normally starts, e.g. "5pm"
+* "stream-end" - The maximum time your stream can end, e.g. "5am"
+* "timer-start" - How much to start the timer with prior to any bits/subs/tips, e.g. "3h"
+* "prime-sub" - How much time should be added for a Twitch Prime Sub, e.g. "30s"
+* "t1-sub" - How much time should be added for a Tier 1 Twitch Sub, e.g. "30s"
+* "t2-sub" - How much time should be added for a Tier 2 Twitch Sub, e.g. "1m"
+* "t3-sub" - How much time should be added for a Tier 2 Twitch Sub, e.g. "2m30s"
+* "per100bits" - How much time should be added for every 100 bits on Twitch, e.g. "6s"
+* "perdollartip" - How much time should be added for dollar of a tip, e.g."6s"
 
 # Use
-Open up a terminal.
+
+Open up a terminal. Navigate to the directory where `server.py` exists.
 Type `python server.py`
 
-It will start a webserver on port 8001, configurable only by editing the script.
+It will start a webserver on port 8001. The port is configurable only by editing the script.
 
 To stop the program, hit CTRL+C.
 
-The server will respond to URLs of the form:
+## /timer
+
+This modifies the file `timer_data.json` which is intended to be paired with timer.html
+
+For URLs of the form:
+
+    http://127.0.0.1:8001/timer?bits=amount
+    http://127.0.0.1:8001/timer?sub=amount&tier=tier
+    http://127.0.0.1:8001/timer?tip=amount
+
+It will increase the end time by an appropriate amount according to the config values.
+In the case of a gift sub or resub, use the amount of 1. The amount is primarily there for community gift subs.
+The acceptable values for tier are "Prime", "Tier 1", "Tier 2", or "Tier 3"
+
+## /write
+
+For URLs of the form:
+
     http://127.0.0.1:8001/write?filename=filename.txt&data=contents
 
 It will open up the filename `filename.txt` and replace the contents with `contents`.
 Unless otherwise specified, the file `filename.txt` will be in the same directory as the script.
+Be careful of what file you specify, if it is important, it will be overwritten.
--- server.py
+++ server.py
@@ -1,34 +1,21 @@
 import http.server
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
+import datetime
+import json
+from dateutil.parser import parse
 
 class WritingHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        # Only answer calls to /write
+        # Handle the request
         url = urlparse(self.path)
-        if url.path != '/write':
+        if url.path == '/write':
+            self.handle_write()
+        elif url.path == '/timer':
+            self.handle_timer()
+        else:
             self.log_message("Ignoring request to " + self.path)
             self.send_error(404)
-
-        # Extract query
-        query = parse_qs(url.query)
-
-        # Require both filename and data
-        if not 'filename' in query:
-            self.log_message("Missing query parameter filename")
-            self.send_error(404)
-        if not 'data' in query:
-            self.log_message("Missing query parameter data")
-            self.send_error(404)
-
-        filename = query['filename'][0]
-        data = query['data'][0]
-
-        # Actually write to the file
-        with open(filename, 'w') as w:
-            w.write(data)
-
-        self.log_message("To the filename '" + filename + "', the contents '" + data + "' was written")
 
         # Write response
         self.send_response(200)
@@ -39,20 +26,158 @@ class WritingHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
 
         return
 
+    def handle_write(self):
+        # Extract query
+        query = parse_qs(urlparse(self.path).query)
+
+        # Require both filename and data
+        if not 'filename' in query:
+            self.log_message("Missing query parameter filename")
+            self.send_error(404)
+            return
+        if not 'data' in query:
+            self.log_message("Missing query parameter data")
+            self.send_error(404)
+            return
+
+        filename = query['filename'][0]
+        data = query['data'][0]
+
+        # Actually write to the file
+        with open(filename, 'w') as w:
+            w.write(data)
+
+        self.log_message("To the file named '" + filename + "', the contents '" + data + "' was written")
+
+    def handle_timer(self):
+        # Extract query
+        query = parse_qs(urlparse(self.path).query)
+
+        # Calculate how much should be added to the timer
+        time_added = 0
+        global config
+        if 'bits' in query:
+            time_added = config['timer']['per100bits'] * int(query['bits'][0]) / 100
+            self.log_message(query['bits'][0] + ' bits adds up to ' + str(time_added) + ' to the timer')
+        elif 'sub' in query and 'tier' in query:
+            if query['tier'][0].lower() == 'prime':
+                time_added = config['timer']['prime-sub'] * int(query['sub'][0])
+                self.log_message(query['sub'][0] + ' prime sub adds up to ' + str(time_added) + ' to the timer')
+            elif query['tier'][0].lower() == 'tier 1':
+                time_added = config['timer']['t1-sub'] * int(query['sub'][0])
+                self.log_message(query['sub'][0] + ' tier 1 subs adds up to ' + str(time_added) + ' to the timer')
+            elif query['tier'][0].lower() == 'tier 2':
+                time_added = config['timer']['t2-sub'] * int(query['sub'][0])
+                self.log_message(query['sub'][0] + ' tier 2 subs adds up to ' + str(time_added) + ' to the timer')
+            elif query['tier'][0].lower() == 'tier 3':
+                time_added = config['timer']['t3-sub'] * int(query['sub'][0])
+                self.log_message(query['sub'][0] + ' tier 3 subs adds up to ' + str(time_added) + ' to the timer')
+            else:
+                self.log_message("Unknown tier of " + query['tier'][0])
+        elif 'tip' in query:
+            time_added = config['timer']['perdollartip'] * float(query['tip'][0])
+            self.log_message(query['tip'][0] + ' tip adds up to ' + str(time_added) + ' to the timer')
+        else:
+            self.log_message("Unknown query")
+            return
+
+        # Read in timer info
+
+        timer_data = {}
+        with open('timer_data.json') as f:
+            timer_data = json.load(f)
+
+        # Determine if change needed
+        if timer_data['current-end'] == timer_data['stream-end']:
+            # Already at max extension
+            self.log_message("Already maxed out timer")
+            return
+
+        # Adding at least some time
+        current_end = datetime.datetime.fromisoformat(timer_data['current-end'])
+        stream_end = datetime.datetime.fromisoformat(timer_data['stream-end'])
+        current_end += time_added
+
+        # Do not exceed stream end
+        if current_end > stream_end:
+            current_end = stream_end
+
+        # Convert back to ISO Format and write out
+        timer_data['current-end'] = current_end.isoformat()
+
+        with open('timer_data.json', 'w') as f:
+            json.dump(timer_data, f, indent=4)
+
+
+
+def readConfig(filename):
+    with open(filename) as f:
+        config = json.load(f)
+
+        # Convert inputs into datetime and timedeltas as appropriate
+        
+        config['timer']['stream-start'] = parse(config['timer']['stream-start'])
+        config['timer']['stream-end'] = parse(config['timer']['stream-end'])
+
+        if config['timer']['stream-end'] < config['timer']['stream-start']:
+            # Assume the stream crosses a midnight boundary
+            config['timer']['stream-end'] += datetime.timedelta(days=1)
+
+        # dateutil.parser will assume today's date if given something like "30s", subtract
+        # off today's date to get a correct duration
+        today_no_time = datetime.datetime.combine(datetime.date.today(), datetime.time())
+        config['timer']['timer-start'] = parse(config['timer']['timer-start']) - today_no_time
+        config['timer']['prime-sub'] = parse(config['timer']['prime-sub']) - today_no_time
+        config['timer']['t1-sub'] = parse(config['timer']['t1-sub']) - today_no_time
+        config['timer']['t2-sub'] = parse(config['timer']['t2-sub']) - today_no_time
+        config['timer']['t3-sub'] = parse(config['timer']['t3-sub']) - today_no_time
+
+        config['timer']['per100bits'] = parse(config['timer']['per100bits']) - today_no_time
+        config['timer']['perdollartip'] = parse(config['timer']['perdollartip']) - today_no_time
+
+        return config
+
+def ensureTimerSetup(config):
+    import os.path
+    if os.path.exists('timer_data.json'):
+        with open('timer_data.json') as f:
+            timer_data = json.load(f)
+
+            # Convert into datetimes
+            timer_data['stream-start'] = parse(timer_data['stream-start'])
+            timer_data['stream-end'] = parse(timer_data['stream-end'])
+            timer_data['current-end'] = parse(timer_data['current-end'])
+
+            now = datetime.datetime.now()
+            if timer_data['stream-start'] < now and now <  timer_data['current-end']:
+                print("Since current time is between start and end in timer_data.json, assuming script restarted during stream")
+                return
+
+    # No existing timer data or expired
+    timer_data = {}
+    timer_data['stream-start'] = config['timer']['stream-start'].isoformat()
+    timer_data['stream-end'] = config['timer']['stream-end'].isoformat()
+    timer_data['current-end'] = (config['timer']['stream-start'] + config['timer']['timer-start']).isoformat()
+
+    with open('timer_data.json', 'w') as f:
+        json.dump(timer_data, f, indent=4)
+
 
 def startServer():
-    HOST = '127.0.0.1'
-    PORT = 8001
-
-    print("Server started on http://" + HOST + ":" + str(PORT))
+    global config
+    config = readConfig('config.json')
+    ensureTimerSetup(config)
+ 
+    print("Server started on http://" + config['host'] + ":" + str(config['port']))
     print("Use CTRL+C to stop")
 
     # Setup server
     handler = WritingHttpRequestHandler
-    server = http.server.HTTPServer((HOST, PORT), handler)
+    server = http.server.HTTPServer((config['host'], config['port']), handler)
 
     # Start the server
     server.serve_forever()
+
 
 if __name__ == "__main__":
     startServer()
