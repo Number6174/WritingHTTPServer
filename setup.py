2d5109e678cbd849e5798f224e87c09dc43f0904
Number6174 <71176286+Number6174@users.noreply.github.com>
2021-08-01 02:10:57 -0600

    Fix path in setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
     },
     data_files = [('.', ['config.json', 'config.json.license', 'control_panel.html', 'README.md']),
                   ('examples', ['examples/timer.html', 'examples/clock.png', 'examples/clock.png.license']),
-                  ('LICENSES', ['LICENSES/CC0-1.0.txt', 'Apache-2.0.txt'])],
+                  ('LICENSES', ['LICENSES/CC0-1.0.txt', 'LICENSES/Apache-2.0.txt'])],
     console=['server.py']
 )
