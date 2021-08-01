fe4f2235138398711db5b909461089454c586a09
Number6174 <71176286+Number6174@users.noreply.github.com>
2021-07-31 18:08:06 -0600

    py2exe release github action

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    options = {
+        'py2exe': {
+            'bundle_files': 1, # Bundle into as few files as possible
+            'optimize': 2, # Apply more optimizations
+            'includes': ['pynput.keyboard._win32', 'pynput.mouse._win32'],
+            'excludes': ['doctest', 'pdb', 'unittest', 'difflib', 'inspect'],
+            'compressed': True,
+        }
+    },
+    data_files = [('.', ['config.json', 'control_panel.html'])],
+    console=['server.py']
+)
